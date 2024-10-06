ffca00ccf2f7c0e15b7552b2dee9b5606763880b
SumiyaRahman <sumiarahman01739@gmail.com>
2024-10-07 03:04:11 +0600

    spinner

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+let sortPets = [];
+
 const adoptSection = document.getElementById("adopt-section");
 document.getElementById("view-btn").addEventListener("click", function () {
   adoptSection.scrollIntoView({ behavior: "smooth" });
@@ -23,7 +25,7 @@ const petCategories = async (categories) => {
         <div onclick="getIndividualPets('${category}')" class="py-5 px-8 rounded-xl border border-[#0e7a8126] flex justify-center cursor-pointer">
             <button class="flex items-center justify-center">
                 <img class="w-14 h-14" src=${category_icon}>
-                <span class="text-2xl font-bold text-secondary ps-4">${category}</span>
+                <span class="text-2xl font-bold text-secondary ps-4">${category}s</span>
             </button>
         </div>
         `;
@@ -40,71 +42,105 @@ const getAllPets = async () => {
 };
 
 const allPets = (pets) => {
+  const spinner = document.getElementById("spinner");
+  const displayAllPet = document.getElementById("display-all-pet");
   const allPetContainer = document.getElementById("all-pets");
   allPetContainer.innerHTML = "";
 
-  if (pets.length == 0) {
-    allPetContainer.classList.remove("grid");
-    allPetContainer.innerHTML = `
-        <div class="bg-[#13131308] py-28 rounded-2xl">
-            <div class="flex justify-center">
-                <img src="./images/error.webp">
-            </div>
-            <div class="text-center pt-6 space-y-4">
-                <h1 class="text-3xl font-bold text-secondary">No Information Available</h1>
-                <p class="text-base text-small-text">No pets available right now, but check back soon for your future furry friend!</p>
-            </div>
-        </div>
-        `;
-    return;
-  } else {
-    allPetContainer.classList.add("grid");
-  }
+  spinner.classList.remove("hidden");
+  displayAllPet.classList.add("hidden");
 
-  pets.forEach((pet) => {
-    const { image, pet_name, breed, date_of_birth, gender, price } = pet;
-    const div = document.createElement("div");
-    div.innerHTML = `
-                        <div class="border p-5 rounded-xl">
-                            <img class="w-full h-full lg:h-[160px] object-cover rounded-xl" src=${image}>
-                            <div>
-                                <h6 class="inter mt-6 mb-2 text-secondary text-xl font-bold">${pet_name}</h6>
-                                <div class="text-base space-y-2">
-                                    <div class="breed flex items-center space-x-2">
-                                        <img src="./images/gird.png">
-                                        <p>Breed: ${breed}</p>
-                                    </div>
-                                    <div class="breed flex items-center space-x-2">
-                                        <img src="./images/calender.png">
-                                        <p>Birth: ${date_of_birth}}</p>
-                                    </div>
-                                    <div class="breed flex items-center space-x-2">
-                                        <img src="./images/gender.png">
-                                        <p>Gender: ${gender}</p>
-                                    </div>
-                                    <div class="breed flex items-center space-x-2">
-                                        <img src="./images/price.png">
-                                        <p>Price: ${price}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr class="my-4 border border-b-[#1313131a]">
-
-                            <div class="flex items-center justify-between">
-                                <div class="border border-[#0e7a8126] rounded-lg">
-                                    <img class="py-2 px-4" src="./images/like.png">
-                                </div>
-                                <div onclick="countDown()">
-                                    <button class="font-semibold text-base text-primary border border-[#0e7a8126] py-2 px-4 rounded-lg">Adopt</button>
-                                </div>
-                                <div>
-                                    <button class="font-semibold text-base text-primary border border-[#0e7a8126] py-2 px-4 rounded-lg">Details</button>
-                                </div>
-                            </div>
-                        </div>
-        `;
-    allPetContainer.append(div);
-  });
+  setTimeout(() => {
+    spinner.classList.add("hidden");
+    displayAllPet.classList.remove("hidden");
+    if (pets.length == 0) {
+      allPetContainer.classList.remove("grid");
+      allPetContainer.innerHTML = `
+          <div class="bg-[#13131308] py-28 rounded-2xl">
+              <div class="flex justify-center">
+                  <img src="./images/error.webp">
+              </div>
+              <div class="text-center pt-6 space-y-4">
+                  <h1 class="text-3xl font-bold text-secondary">No Information Available</h1>
+                  <p class="text-base text-small-text">No pets available right now, but check back soon for your future furry friend!</p>
+              </div>
+          </div>
+          `;
+      return;
+    } else {
+      allPetContainer.classList.add("grid");
+    }
+  
+    const sortedPetsByPrice = pets
+      .map((sortPet) => ({
+        petId: sortPet.petId,
+        pet_name: sortPet.pet_name,
+        gender: sortPet.gender,
+        image: sortPet.image,
+        price: sortPet.price,
+        date_of_birth: sortPet.date_of_birth,
+        category: sortPet.category,
+        breed: sortPet.breed,
+      }))
+      .sort((x, y) => y.price - x.price);
+  
+      sortPets = sortedPetsByPrice;
+  
+    pets.forEach((pet) => {
+      const { petId, image, pet_name, breed, date_of_birth, gender, price } = pet;
+      const div = document.createElement("div");
+      div.innerHTML = `
+                          <div class="border p-5 rounded-xl">
+                              <img class="w-full h-full lg:h-[160px] object-cover rounded-xl" src=${image}>
+                              <div>
+                                  <h6 class="inter mt-6 mb-2 text-secondary text-xl font-bold">${pet_name}</h6>
+                                  <div class="text-base space-y-2 text-small-text">
+                                      <div class="breed flex items-center space-x-2">
+                                          <img src="./images/gird.png">
+                                          <p>Breed: ${
+                                            !breed ? "Unknown" : breed
+                                          }</p>
+                                      </div>
+                                      <div class="breed flex items-center space-x-2">
+                                          <img src="./images/calender.png">
+                                          <p>Birth: ${
+                                            !date_of_birth
+                                              ? "Unknown"
+                                              : date_of_birth.slice(0,4)
+                                          }</p>
+                                      </div>
+                                      <div class="breed flex items-center space-x-2">
+                                          <img src="./images/gender.png">
+                                          <p>Gender: ${
+                                            !gender ? "Unknown" : gender
+                                          }</p>
+                                      </div>
+                                      <div class="breed flex items-center space-x-2">
+                                          <img src="./images/price.png">
+                                          <p>Price: ${
+                                            !price ? "Unknown" : price
+                                          }$</p>
+                                      </div>
+                                  </div>
+                              </div>
+                              <hr class="my-4 border border-b-[#1313131a]">
+  
+                              <div class="flex items-center justify-between">
+                                  <div onclick="likedPet('${image}')" class="border border-[#0e7a8126] rounded-lg cursor-pointer">
+                                      <img class="py-2 px-4" src="./images/like.png">
+                                  </div>
+                                  <div onclick="countDown()">
+                                      <button class="font-semibold text-base text-primary border border-[#0e7a8126] py-2 px-4 rounded-lg">Adopt</button>
+                                  </div>
+                                  <div onclick="showDetails(${petId})">
+                                      <button class="font-semibold text-base text-primary border border-[#0e7a8126] py-2 px-4 rounded-lg">Details</button>
+                                  </div>
+                              </div>
+                          </div>
+          `;
+      allPetContainer.append(div);
+    });
+  }, 2000)
 };
 
 const getIndividualPets = async (category) => {
@@ -116,9 +152,85 @@ const getIndividualPets = async (category) => {
   allPets(data.data);
 };
 
-const countDown = () => {
-  const showCountdown = 
+const likedPet = async (image) => {
+  const likedPets = document.getElementById("liked-pets");
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div>
+    <img class="rounded-xl" src=${image}>
+  </div>
+  `;
+
+  likedPets.append(div);
 };
+
+const showDetails = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/peddy/pet/${id}`
+  );
+  const data = await response.json();
+
+  const modalContainer = document.getElementById("modal-container");
+  const {
+    image,
+    pet_name,
+    breed,
+    date_of_birth,
+    gender,
+    price,
+    vaccinated_status,
+    pet_details,
+  } = data.petData;
+  modalContainer.innerHTML = `
+  <img class="w-full rounded-xl" src=${image} />
+          <h3 class="text-2xl font-bold mt-6 mb-4">${pet_name}</h3>
+          <div class="flex text-sm text-small-text">
+            <div class="space-y-2">
+              <div class="breed flex items-center space-x-2">
+                <img src="./images/gird.png" />
+                <p>Breed: ${!breed ? "Unknown" : breed}</p>
+              </div>
+              <div class="breed flex items-center space-x-2">
+                <img src="./images/gender.png" />
+                <p>Gender: ${!gender ? "Unknown" : gender}</p>
+              </div>
+              <div class="breed flex items-center space-x-2">
+                <img src="./images/gender.png" />
+                <p>Vaccinated status: ${
+                  vaccinated_status ? "Unknown" : vaccinated_status
+                }</p>
+              </div>
+            </div>
+            <div class="space-y-2">
+              <div class="breed flex items-center space-x-2">
+                <img src="./images/calender.png" />
+                <p>Birth: ${!date_of_birth ? "Unknown" : date_of_birth.slice(0,4)}</p>
+              </div>
+              <div class="breed flex items-center space-x-2">
+                <img src="./images/price.png" />
+                <p>Price: ${!price ? "Unknown" : price}$</p>
+              </div>
+            </div>
+          </div>
+          <hr class="my-4 border border-b-[#1313131a]">
+          <div class="mb-4">
+            <h5 class="text-base font-semibold text-secondary">Details Information</h5>
+            <p class="text-sm text-small-text mt-3">${pet_details}</p>
+          </div>
+          <div class="modal-action w-full">
+            <form class="w-full" method="dialog">
+              <button class="py-3 w-full border border-[#0e7a8133] text-primary font-bold rounded-lg bg-[#0e7a811a]">Cancel</button>
+            </form>
+          </div>
+  `;
+  document.getElementById("my_modal_1").showModal();
+};
+
+const showSortedPets = () => {
+  allPets(sortPets);
+}
+
 
 getAllPets();
 getPetByCategories();
